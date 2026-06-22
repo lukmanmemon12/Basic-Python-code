1d8a0e56246ae45446b48c8a9c1c6e4d9ef198e0
Lukman <lukmanmemon169@gmail.com>
2026-06-22 15:49:44 +0530

    Day 1
    Nested if

---
+++ Nested if.py
@@ -0,0 +1,11 @@
+n1 = 11
+if n1>0:
+    if n1==10:
+        print("n1 is 10")
+    elif n1>10:
+        print("n1 is >10")
+    else:
+        print("n1 is <10")
+else:
+    print("ENter Positive value")         
+                     
