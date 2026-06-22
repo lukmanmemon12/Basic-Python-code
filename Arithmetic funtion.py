a0721f6dd9a57f27c972cbb9018488dac5fb78d9
Lukman <lukmanmemon169@gmail.com>
2026-06-22 15:22:55 +0530

    day 1
    Python code

---
+++ Arithmetic funtion.py
@@ -0,0 +1,27 @@
+x = 10
+y =5
+
+# addition
+print("sum:", x+y)
+
+#subtraction
+
+print("subtration: ",x -y)
+
+#multiplication
+
+print("multiplication:", x * y)
+
+# division
+
+print("division:",x / y)
+
+#floor division
+print("floor division:", x//y)
+
+#modulo
+
+print("odulo:",x%y)
+
+#  x to the power y
+print("Powern: ", x**y)
